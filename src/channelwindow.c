15e9cd96c03ec5275bc49f39a7af294729995959
turcja <james@finebyte.co.uk>
2015-05-27 20:43:51 +0100

    Support REFRESH of channels / unread count

--- src/channelwindow.c
+++ src/channelwindow.c
@@ -17,6 +17,7 @@ static Window *window=NULL;
 static MenuLayer *menu_layer;
 static TextLayer  * watchInfo;
 static TitleLayer * title_layer;
+static AppTimer * refresh_chan_timer;
 
 
 
@@ -47,13 +48,13 @@ void menu_select_callback(MenuLayer *menu_layer, MenuIndex *cell_index, void *da
 	chatwindow_create(&channels[cell_index->section].chans[cell_index->row]);
 }
 
- int16_t menu_get_header_height_callback( MenuLayer *menu_layer, uint16_t section_index, void *callback_context) {
-	 return MENU_CELL_BASIC_HEADER_HEIGHT;
- }
+int16_t menu_get_header_height_callback( MenuLayer *menu_layer, uint16_t section_index, void *callback_context) {
+	return MENU_CELL_BASIC_HEADER_HEIGHT;
+}
 
- void menu_draw_header_callback(GContext *ctx, const Layer *cell_layer, uint16_t section_index, void *callback_context) {
-		menu_cell_basic_header_draw(ctx,cell_layer,sectionTitles[section_index]);
- }
+void menu_draw_header_callback(GContext *ctx, const Layer *cell_layer, uint16_t section_index, void *callback_context) {
+	menu_cell_basic_header_draw(ctx,cell_layer,sectionTitles[section_index]);
+}
 
 // This initializes the menu upon window load
 void window_load(Window *window) {
@@ -72,7 +73,7 @@ void window_load(Window *window) {
 	menu_layer_set_callbacks(menu_layer, NULL, (MenuLayerCallbacks){
 		.get_num_sections = menu_get_num_sections_callback,
 				.get_header_height = menu_get_header_height_callback,
-		.get_num_rows = menu_get_num_rows_callback,
+				.get_num_rows = menu_get_num_rows_callback,
 				.draw_row = menu_draw_row_callback,
 				.draw_header = menu_draw_header_callback,
 				.select_click = menu_select_callback,
@@ -106,32 +107,59 @@ void window_unload(Window *window) {
 void channelwindow_create() {
 
 	if (window==NULL) {
-	window = window_create();
+		window = window_create();
 #ifdef PBL_SDK_2
-	window_set_fullscreen(window,true);
+		window_set_fullscreen(window,true);
 #endif
 
-	// Setup the window handlers
-	window_set_window_handlers(window, (WindowHandlers) {
-		.appear = &window_load,
-				.disappear = &window_disappear,
-				.unload = &window_unload
-	});
+		// Setup the window handlers
+		window_set_window_handlers(window, (WindowHandlers) {
+			.appear = &window_load,
+					.disappear = &window_disappear,
+					.unload = &window_unload
+		});
 
 
-	window_stack_push(window,true);
+		window_stack_push(window,true);
 	} else {
 		menu_layer_reload_data(menu_layer);
 		layer_mark_dirty(window_get_root_layer(window));
 	}
 }
 
+#define CHAN_REFESH_INTERVAL 5*60*1000
+
+void refresh_chans(void * data) {
+
+	APP_LOG(APP_LOG_LEVEL_DEBUG,"REFRESH CHANS");
+	sendCommand("REFRESH","ALL");
+
+	app_timer_reschedule(refresh_chan_timer, CHAN_REFESH_INTERVAL);
+	//	refresh_timer=app_timer_register(2*60*1000,refresh_chan_timer,NULL);
+
+}
+
+
+void delChannels(int id) {
+	APP_LOG(APP_LOG_LEVEL_DEBUG,"DELCHANS");
+	int j;
+	for (j=0;j<channels[id].num;j++) {
+		chan_info * c = &channels[id].chans[j];
+		if (c->id) free(c->id);
+		if (c->name) free(c->name);
+		if (c->unread_msg) free(c->unread_msg);
+	}
+	if (channels[id].chans) free(channels[id].chans);
+}
+
 
 
 void addChannels(char * v, int id) {
 
-	char * m=strdup(v);
-	APP_LOG(APP_LOG_LEVEL_DEBUG,"addChannels %s", m);
+	APP_LOG(APP_LOG_LEVEL_DEBUG,"addChannels %s", v);
+
+	delChannels(id);
+
 	if (v!=NULL) {
 		char * tok = strtok(v,SEP);
 		if (tok!=NULL) {
@@ -142,8 +170,7 @@ void addChannels(char * v, int id) {
 		if (channels[id].num > 0) {
 			uint8_t i = 0;
 			tok=strtok(NULL,SEP);
-			APP_LOG(APP_LOG_LEVEL_DEBUG,"next chan id %s", tok);
-			// assuming correct formed triples...
+			// assuming correct formed messages...
 			while (tok!=NULL) {
 				APP_LOG(APP_LOG_LEVEL_DEBUG,"next chan id %s", tok);
 				channels[id].chans[i].id = strdup(tok);
@@ -158,12 +185,14 @@ void addChannels(char * v, int id) {
 			}
 		}
 	}
-	free(m);
 
 	static char time_text[] = "Updated at HH:MM";
 	time_t tt = time(NULL);
 	struct tm* curret_time = localtime(&tt);
 	strftime(time_text, sizeof(time_text), "Updated at %H:%M", curret_time);
 	channelWindowTitle = time_text;
+
+	refresh_chan_timer = app_timer_register(CHAN_REFESH_INTERVAL, refresh_chans,NULL);
+
 
 }
